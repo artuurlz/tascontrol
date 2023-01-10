958305a5dbeacde9c3900367e844e174bb13dccb
artuurlz <arturcontatop@gmail.com>
2023-01-09 21:13:11 -0300

    Atualização no sistema de adição de task

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-var metaUser = parseInt(prompt('Qual a sua meta de tasks diária?'));
+let metaUser = parseInt(prompt('Qual a sua meta de tasks diária?'));
+let pergunta = confirm('Já fez alguma task hoje? Se sim confirme e digite a quantidade!');
+let newValue = 0;
 
 document.querySelector('#home_box_result_meta_value').innerHTML=metaUser;
 
-var totaltask = parseInt(document.querySelector('#home_box_result_totaltask_value').innerHTML);
-var faltatask = parseInt(document.querySelector('#home_box_result_falta_value').innerHTML=metaUser);
-var money = parseFloat(document.querySelector('#money').innerHTML);
-var taskvalue = 0.03;
+if(pergunta === true){
+    newValue = parseInt(prompt('Digite a quantidade já feita'));
+};
 
-var btnAdc1 = document.querySelector('#schedule_1');
-var btnRem1 = document.querySelector('#schedule_remove_1');
-var valor1 = parseInt(document.querySelector('.home_box_schedule_title_1_value').innerHTML);
+let taskvalue = 0.03;
+let totaltask = parseInt(document.querySelector('#home_box_result_totaltask_value').innerHTML=newValue);
+let faltatask = parseInt(document.querySelector('#home_box_result_falta_value').innerHTML=(metaUser-newValue));
+let money = parseFloat(document.querySelector('#money').innerHTML=(newValue*taskvalue));
+
+let btnAdc1 = document.querySelector('#schedule_1');
+let btnRem1 = document.querySelector('#schedule_remove_1');
+let valor1 = parseInt(document.querySelector('.home_box_schedule_title_1_value').innerHTML=newValue);
 
 if(btnAdc1.addEventListener('click', () => {
     document.querySelector('.home_box_schedule_title_1_value').innerHTML=++valor1;
